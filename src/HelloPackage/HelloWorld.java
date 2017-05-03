0ecb1208d83e495728e84cab472254abbe6662b4
Patrick Curtis <Paralolagram@gmail.com>
2017-05-02 23:48:44 -0700

    added a cute message to the hello world

--- src/HelloPackage/HelloWorld.java
+++ src/HelloPackage/HelloWorld.java
@@ -1,10 +1,13 @@
 package HelloPackage;
 
+
 /**
  * Created by patma on 5/2/2017.
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("how are you doing world?");
+
     }
 }
